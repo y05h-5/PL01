a18853f06dcadbcee175d766362b4255a7f94844
y05h-5 <syoshiki424@gmail.com>
2021-12-23 21:45:20 +0900

    got it

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <math.h>
 
-#define VALID           1
-#define INVALID         0
-#define COORDS_PER_LINE 2
-#define NUM_LINE        1
+#define VALID      1
+#define INVALID    0
+#define NUM_COORDS 2
+// #define NUM_LINE        1
 
+
+/* ### STRUCTS & ENUMS ### */
 typedef struct {
     int x,y;
 } Point;
-typedef struct {
-    Point coords[COORDS_PER_LINE];
-} Line;
+// typedef struct {
+//     Point coords[NUM_COORDS];
+// } Line;
 
 #define X_ENUM(X) X,
 #define X_STRING(X) #X,
@@ -28,14 +31,12 @@ enum ErrorType {
 };
 typedef enum ErrorType InputErrorType;
 typedef enum ErrorType CoordErrorType;
+
 const char* ErrorNames[NUM_ERRORS+1] = { ERROR_TYPE_TABLE(X_STRING) };
+/* ### END OF STRUCTS & ENUMS ### */
 
-void print_coord(const Point* crd) {
-    printf("\n");
-    for (int i = 0; i < COORDS_PER_LINE; ++i)
-        printf("coordinates #%d = (%d, %d)\n",i+1, crd[i].x, crd[i].y);
-}
 
+/* ### INPUT & INPUT ERROR FUNCTIONS ### */
 void input_error_message(const InputErrorType type) {
     printf("\nERROR: %s\n", ErrorNames[type]);
     switch (type) {
@@ -44,7 +45,7 @@ void input_error_message(const InputErrorType type) {
     break;
     case TOO_MANY: 
         printf("    Too many inputs were given.\n");
-        printf("    Expected number of inputs = %d\n", COORDS_PER_LINE);
+        printf("    Expected number of inputs = %d\n", NUM_COORDS);
     break;
     default: 
         printf("If you are seeing this message, something is wrong with the code.\n");
@@ -69,10 +70,10 @@ int8_t input_coords(Point* crd) {
     InputErrorType error = NO_ERROR;
 
     printf("Input coordinate points (x y)\n");
-    for (int i = 0; i < COORDS_PER_LINE; ++i) {
+    for (int i = 0; i < NUM_COORDS; ++i) {
         printf("Input coordinate #%d (x y): ",i+1);
         retScan = scanf("%d %d", &crd[i].x, &crd[i].y);
-        error = (retScan!=COORDS_PER_LINE) ? DATA_TYPE : validate_input(crd[i]);
+        error = (retScan!=NUM_COORDS) ? DATA_TYPE : validate_input(crd[i]);
         if (error) {
             input_error_message(error);
             return INVALID;
@@ -80,7 +81,10 @@ int8_t input_coords(Point* crd) {
     }
     return VALID;
 }
+/* ### END OF INPUT FUNCTIONS ### */
 
+
+/* ### COORDINATES FUNCTIONS ### */
 void coord_error_message(const CoordErrorType type) {
     printf("\nERROR: %s\n", ErrorNames[type]);
     switch (type) {
@@ -104,8 +108,8 @@ int8_t validate_coords(const Point* crd) {
     CoordErrorType error = NO_ERROR;
 
     printf("\nchecking coordinates...\n");
-    for (int i = 0; i < COORDS_PER_LINE-1; ++i) {
-        for (int j = i+1; j < COORDS_PER_LINE; ++j) {
+    for (int i = 0; i < NUM_COORDS-1; ++i) {
+        for (int j = i+1; j < NUM_COORDS; ++j) {
             if (crd[i].x == crd[j].x && crd[i].y == crd[j].y)
                 error = OVERLAP;
             else if (crd[i].y == crd[j].y) 
@@ -123,27 +127,82 @@ int8_t validate_coords(const Point* crd) {
     return VALID;
 }
 
+void print_coord(const Point* crd) {
+    printf("\n");
+    for (int i = 0; i < NUM_COORDS; ++i)
+        printf("coordinates #%d = (%d, %d)\n",i+1, crd[i].x, crd[i].y);
+}
+/* ### END OF COORDINATES FUNCTIONS ### */
+
+
+/* ### CALCULATION FUNCTIONS ### */
 void get_standardform(const Point* crd) {
-    int dy = crd[1].y - crd[0].y;  // include this in the for loop when dealing with more than 1 lines???
-    int dx = crd[1].x - crd[0].x;
-    float slopes[NUM_LINE] = {0.0f};
-    slopes[0] = dy/dx;
+    // assume there is only one line 
+    // and a line has only two points
+    /***
+     * int x1,y1 : coordinates of the first point
+     * int x2,y2 : coordinates of the second point
+     * int dx,dy : represents delta x and delta y
+     * 
+     * float slope = slope of the line constructed by 2 points
+     */
+    int   x1 = crd[0].x, y1 = crd[0].y;
+    int   x2 = crd[1].x, y2 = crd[1].y; 
+    float dy = y2 - y1,  dx = x2 - x1;
+    float slope = dy/dx;
 
     printf("\nfinding the standard form equation (Ax + By + C = 0)...\n");
+    printf("slope of the line = %f\n", slope);
+    for (int i = 0; i < NUM_COORDS; ++i) {
+        // use both coordinates to make sure the answer is right
+        printf("\nusing coordinate #%d\n",i+1); 
 
-    for (int i = 0; i < COORDS_PER_LINE; ++i) {
-        printf("using coordinate #%d\n",i+1);
-        float a  = slopes[0], b = 1.0f;
-              a *= crd[i].x;
-        float c  = a - crd[i].y;
-        printf("Result:\n" \
-               "%fx + %fy + %f = 0\n", a,b,c);
+        /***
+         * standard form equation: Ax + By + C = 0
+         * float a : represents 'A', which is the coefficient of x
+         * float b : represents 'B', which is the coefficient of x
+         * float c : represents 'C', which is the constant in the equation
+         * 
+         * char operator1&2 : '+' or '-' characters in the equation. changes according to 
+         *                    the signs of float variables b and c to make the result 
+         *                    look better.
+         */
+        float a = 0.0f, b = 0.0f, c = 0.0f;
+        char operator1 = '+', operator2 = '+';
+        
+        /***
+         * calculating a,b and c by...
+         *  1) y - y1 = slope(x - x1)
+         *  2) y - y1 = slope*x - slope*x1
+         *  3) y - slope*x -y1 + slope*x1 = 0
+         * so result will look like...
+         *     by - slope*x + (-y1 + slope*x1) = 0
+         * where...
+         *     a = -(slope*x)
+         *     b = 1
+         *     c = (slope*x1 - y1)
+         */
+        a = (-1) * slope;
+        b = 1.0f;
+        c = slope * crd[i].x - crd[i].y;
+        printf("A = %f\n",a);
+        printf("B = %f\n",b);
+        printf("C = %f\n",c);
+
+        operator1 = (b < 0)? '-' : '+';
+        operator2 = (c < 0)? '-' : '+';
+        b = fabs(b);
+        c = fabs(c);
+
+        printf("\nResult:\n"              \
+               "%fx %c %fy %c %f = 0\n",\
+               a,operator1,b,operator2,c);
     }
 }
+/* ### END OF CALCULATION FUNCTIONS ### */
 
 int main(void) {
-    Point coordinates[COORDS_PER_LINE] = {0};
-    Line lines[NUM_LINE] = {0};
+    Point coordinates[NUM_COORDS] = {0};
     int8_t valid_input = INVALID, valid_coords = INVALID;
 
     valid_input = input_coords(coordinates);
