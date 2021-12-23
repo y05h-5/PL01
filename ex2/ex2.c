428cb71c6f3a99492d576c7fbddb5f5bf0aa29b2
y05h-5 <syoshiki424@gmail.com>
2021-12-23 20:24:22 +0900

    get'em x macros

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -10,21 +10,25 @@
 typedef struct {
     int x,y;
 } Point;
-
 typedef struct {
     Point coords[COORDS_PER_LINE];
 } Line;
 
+#define X_ENUM(X) X,
+#define X_STRING(X) #X,
+
+#define ERROR_TYPE_TABLE(X)    \
+    X(NO_ERROR)   X(DATA_TYPE) \
+    X(TOO_MANY)   X(ZERO_SLOPE)\
+    X(ASYMPTOTE)  X(OVERLAP)   \
+    X(NUM_ERRORS)
+
 enum ErrorType {
-    NO_ERROR=0,
-    DATA_TYPE,
-    TOO_MANY,
-    ZERO_SLOPE,
-    ASYMPTOTE,
-    OVERLAP
+    ERROR_TYPE_TABLE(X_ENUM)
 };
 typedef enum ErrorType InputErrorType;
 typedef enum ErrorType CoordErrorType;
+const char* ErrorNames[NUM_ERRORS+1] = { ERROR_TYPE_TABLE(X_STRING) };
 
 void print_coord(const Point* crd) {
     printf("\n");
@@ -33,20 +37,20 @@ void print_coord(const Point* crd) {
 }
 
 void input_error_message(const InputErrorType type) {
-    printf("\nERROR: ");
+    printf("\nERROR: %s\n", ErrorNames[type]);
     switch (type) {
     case DATA_TYPE: 
-        printf("Input contains invalid data type.\n");
+        printf("    Input contains invalid data type.\n");
     break;
     case TOO_MANY: 
-        printf("Too many inputs were given.\n");
-        printf("       Expected number of inputs = %d\n", COORDS_PER_LINE);
+        printf("    Too many inputs were given.\n");
+        printf("    Expected number of inputs = %d\n", COORDS_PER_LINE);
     break;
     default: 
         printf("If you are seeing this message, something is wrong with the code.\n");
         exit(EXIT_FAILURE);
     };
-    printf("       Please check your input.\n");
+    printf("    Please check your input.\n");
 }
 
 InputErrorType validate_input(const Point crd) {
@@ -67,7 +71,7 @@ int8_t input_coords(Point* crd) {
     printf("Input coordinate points (x y)\n");
     for (int i = 0; i < COORDS_PER_LINE; ++i) {
         printf("Input coordinate #%d (x y): ",i+1);
-        retScan = scanf("%d %d", crd[i].x, crd[i].y);
+        retScan = scanf("%d %d", &crd[i].x, &crd[i].y);
         error = (retScan!=COORDS_PER_LINE) ? DATA_TYPE : validate_input(crd[i]);
         if (error) {
             input_error_message(error);
@@ -78,22 +82,22 @@ int8_t input_coords(Point* crd) {
 }
 
 void coord_error_message(const CoordErrorType type) {
-    printf("\nERROR: ");
+    printf("\nERROR: %s\n", ErrorNames[type]);
     switch (type) {
     case ZERO_SLOPE: 
-        printf("The line has no slope (parallel to the x-axis)\n");
+        printf("    The line has no slope (parallel to the x-axis)\n");
     break;
     case ASYMPTOTE:
-        printf("The line is an asymptote (parallel to the y-axis)\n");
+        printf("    The line is an asymptote (parallel to the y-axis)\n");
     break;
     case OVERLAP:
-        printf("Two points overlaps.\n");
+        printf("    Two points overlaps.\n");
     break;
     default: 
         printf("If you are seeing this message, something is wrong with the code.\n");
     exit(EXIT_FAILURE);
     };
-    printf("\nInvalid coordinates.\n");
+    printf("    Invalid coordinates.\n");
 }
 
 int8_t validate_coords(const Point* crd) {
@@ -123,16 +127,17 @@ void get_standardform(const Point* crd) {
     int dy = crd[1].y - crd[0].y;  // include this in the for loop when dealing with more than 1 lines???
     int dx = crd[1].x - crd[0].x;
     float slopes[NUM_LINE] = {0.0f};
-    slope[0] = dy/dx;
+    slopes[0] = dy/dx;
 
     printf("\nfinding the standard form equation (Ax + By + C = 0)...\n");
 
     for (int i = 0; i < COORDS_PER_LINE; ++i) {
         printf("using coordinate #%d\n",i+1);
-        float a = slope, b = 1.0f;
-        float mx = a * crd[i].x;
-        float c  = mx - crd[i].y;
-
+        float a  = slopes[0], b = 1.0f;
+              a *= crd[i].x;
+        float c  = a - crd[i].y;
+        printf("Result:\n" \
+               "%fx + %fy + %f = 0\n", a,b,c);
     }
 }
 
