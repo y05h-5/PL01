cade729f091b1b38b90f0c9602dae3297882ab14
y05h-5 <syoshiki424@gmail.com>
2022-01-13 21:10:47 +0900

    ho4 done

--- ho4/ho4.c
+++ ho4/ho4.c
@@ -3,54 +3,61 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define VALID 1
-#define INVALID 0
-#define NUM_COEFFS 3
+// macros to avoid magic numbers
+#define VALID        1
+#define INVALID      0
+#define NUM_COEFFS   3
 #define NUM_ROOT_MAX 2
+#define TORELANCE_F  1e-6
 
+// macros for defining enums/names
 #define X_ENUM(X)    X,
 #define X_STRING(X) #X,
-#define ERRORTYPE_TABLE(X) \
-    X(NO_ERROR)        X(DATA_TYPE)  \
-    X(TOO_MANY)        X(NUM_ERRORS)
+
+// macro containing all error types
+#define ERRORTYPE_TABLE(X)       \
+    X(NO_ERROR)     X(DATA_TYPE) \
+    X(TOO_MANY)     X(NUM_ERRORS)
 
 typedef enum {
+    // error type enum, for readability
     ERRORTYPE_TABLE(X_ENUM)
 } ErrorType;
 
+// names of errors in string, used for printing them on display
 const char* ErrorNames[NUM_ERRORS+1] = {ERRORTYPE_TABLE(X_STRING)};
 
+// macro containing all equaton types
 #define EQUATION_TYPE_TABLE(X) \
-    X(QUADRATIC)       X(LINEAR) \
-    X(CONSTANT)        X(ZERO)   \
+    X(QUADRATIC)    X(LINEAR)  \
+    X(CONSTANT)     X(ZERO)    \
     X(NUM_EQ_TYPES)
 
 typedef enum {
+    // equation type enum, again for readability
     EQUATION_TYPE_TABLE(X_ENUM)
 } EquationType;
 
+// names of equation types in string
 const char* EquationTypeNames[NUM_EQ_TYPES+1] = {EQUATION_TYPE_TABLE(X_STRING)};
 
+// structure for containing all 3 coefficients in one place
 typedef struct {
     double coef_x2, coef_x, constant;
 } QuadraticCoefficients;
 
+/******* function definitions *******/
+// error messages for all types of errors
 void input_error_message(const ErrorType type) {
     printf("\nERROR: %s\n", ErrorNames[type]);
 
     switch (type) {
-    case DENOM_ZERO:
-        printf("       Division by 0 is not allowed.\n");
-    break;
-    case DENOM_NEGATIVE:
-        printf("       Denominator needs to be a positive integer value.\n");
-    break;
     case DATA_TYPE:
         printf("       Input contains invalid data type.\n");
     break;
     case TOO_MANY:
         printf("       Too many inputs were given.\n");
-        printf("       Expected number of inputs = %d\n",NUM_ELEMENTS);
+        printf("       Expected number of inputs = %d\n",NUM_COEFFS);
     break;
     default:
         printf("If you are seeing this message, something is wrong with the code.\n");
@@ -59,27 +66,27 @@ void input_error_message(const ErrorType type) {
     printf("\n");
 }
 
-ErrorType input_validation(const QuadraticCoefficients input) {
+// validating inputs
+ErrorType input_validation(/*const QuadraticCoefficients* input*/) {
     char invisible_input = getchar();
 
     while (invisible_input!='\n') {
         if (invisible_input!=' ' && invisible_input!='\n')
             return TOO_MANY;
-
         invisible_input = getchar();
     }
 
     return NO_ERROR;
 }
 
+// function to get user inputs and print error messages if there's any error
 int8_t get_coefficients(QuadraticCoefficients* coefs) {
     int retScanf = 0;
     ErrorType error = NO_ERROR;
 
     retScanf = scanf("%lf %lf %lf",&coefs->coef_x2,&coefs->coef_x,&coefs->constant);
-
-    error = (retScanf!=NUM_COEFFS)? DATA_TYPE : input_validation(coefs);
-
+    
+    error = (retScanf!=NUM_COEFFS)? DATA_TYPE : input_validation(/*coefs*/);
     if(error) {
         input_error_message(error);
         return INVALID;
@@ -87,45 +94,58 @@ int8_t get_coefficients(QuadraticCoefficients* coefs) {
     return VALID;
 }
 
-EquationType get_eq_type(const QuadraticCoefficients coefs) {
+// function for figuring out which type of equation was given
+EquationType get_eq_type(const QuadraticCoefficients* coefs) {
     EquationType type = QUADRATIC;
-    if (coefs.coef_x2 == 0)  type = LINEAR;
-    if (coefs.coef_x == 0 && type == LINEAR)
+    if (coefs->coef_x2 == 0) 
+        type = LINEAR;
+    if (coefs->coef_x == 0 && type == LINEAR)
         type = CONSTANT;
-    if (coefs.constant == 0 && type == CONSTANT)
+    if (coefs->constant == 0 && type == CONSTANT)
         type = ZERO;
 
-    printf("Equation Type = %s\n",EquationTypeNames[type]);
+    printf("\nEquation Type = %s\n",EquationTypeNames[type]);
 
     return type;
 }
 
+// print roots of the equation
 void print_roots(int real, double* roots, int num_roots) {
-
+    if (!real) printf("\nroots are imaginary\n");
+    for (int i = 0; i < num_roots; ++i) {
+        if(real) printf("root #%d = %lf\n", i+1, roots[i]);
+        else     printf("root #%d = %lfi\n", i+1, roots[i]);
+    }
 }
 
 // find roots of ax^2 + bx + c = 0
-void solve_quadratic(const QuadraticCoefficients coefs, double* roots) {
+void solve_quadratic(const QuadraticCoefficients* coefs, double* roots) {
     int num_roots = 0;
     
     // compute discriminant
-    double operand = pow(coefs.coef_x2,2) - 4*coefs.coef_x*coefs.constant;
-    int neg_operand = (operand > 0.0)? 0 : 1; // determine if the solution is real
+    double operand = pow(coefs->coef_x2,2) - 4*coefs->coef_x*coefs->constant;
+    int real = (operand > 0.0)? 0 : 1; // determine if the solution is real
     double d = sqrt(fabs(operand));
-    // compute roots by usign discriminant
-    double x1 = (-b+d) / (2*a);
-    double x2 = (-b-d) / (2*a);
+    
+    // compute roots by using the discriminant
+    roots[0] = (-coefs->coef_x+d) / (2*coefs->coef_x2);
+    roots[1] = (-coefs->coef_x-d) / (2*coefs->coef_x2);
+
+    num_roots = (fabs(roots[0]-roots[1]) < TORELANCE_F)? 1 : 2; 
+
+    print_roots(real, roots, num_roots);
 }
 
 // find roots of bx + c = 0
-void solve_linear(QuadraticCoefficients coefs, double* roots) {
+void solve_linear(const QuadraticCoefficients* coefs, double* roots) {
+    roots[0] = -1.0f * coefs->coef_x / coefs->constant;
+    
+    int num_roots = 1, real = 1;
+    print_roots(real, roots, num_roots);
 }
 
-// find roots of c = 0
-void solve_constant(QuadraticCoefficients coefs, double* roots) {
-}
-
-void calculate_roots(const QuadraticCoefficients coefs, EquationType type) {
+// procedure to call different of solver procedues for different tyupes of equations 
+void calculate_roots(const QuadraticCoefficients* coefs, EquationType type) {
     int num_roots = 0;
     double roots[NUM_ROOT_MAX] = {0.0};
 
@@ -140,7 +160,7 @@ void calculate_roots(const QuadraticCoefficients coefs, EquationType type) {
         break;
     case CONSTANT:
         // when c = 0
-        solve_constant(coefs, roots);
+        printf("There is no solution for the given equation.\n");
         break;
     case ZERO:
         // when 0 = 0
@@ -151,7 +171,10 @@ void calculate_roots(const QuadraticCoefficients coefs, EquationType type) {
         exit(EXIT_FAILURE);        
         break;
     }
+    printf("\n");
 }
+
+/******* end of functions ******/
 
 int main() {
     QuadraticCoefficients coefficients = {0.0};
@@ -159,11 +182,11 @@ int main() {
     EquationType type = QUADRATIC;
 
     printf("Roots of ax^2 + bx + c = 0, enter a, b and c: ");
-    valid_input = get_coefficients(&a, &b, &c);
+    valid_input = get_coefficients(&coefficients);
     if (!valid_input) exit(EXIT_FAILURE);
 
-    type = get_eq_type(coefficients);
-    calculate_roots(coefficients, type);
+    type = get_eq_type(&coefficients);
+    calculate_roots(&coefficients, type);
 
     return EXIT_SUCCESS;
 }
