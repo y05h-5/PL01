81bad36ff88a1ec510c96868ccddd651010faf52
y05h-5 <syoshiki424@gmail.com>
2022-01-31 15:45:28 +0900

    ho8

--- ho8/primes/main.c
+++ ho8/primes/main.c
@@ -1,46 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fillPrimes( int* arPrimes, int size );
-void printArray( FILE *out, int* array, int size );
+void fillPrimes(FILE* out, int* arPrimes, int size);
+void printArray(FILE *out, int* array, int size);
 
-#define NumPrimes 1000
+#define NUMPRIMES 20
 
-int main( int argc, char* argv[] )
-{
-	printf( "Running: %s\n", argv[0]);
+int main(int argc, char* argv[]) {
+	printf("Running: %s\n", argv[0]);
 	
-	int arPrimes[NumPrimes];
+	int arPrimes[NUMPRIMES] = {0};
 	FILE *out;
 	
-	if (argc >= 2)
-	{
-		out = fopen(argv[1], "wt");
-	}
-	else
-	{
-		out = fopen("output.txt", "wt");
-	}
+	if (argc >= 2) out = fopen(argv[1], "wt");
+	else out = fopen("output.txt", "wt");
 	
-	if (out == NULL)
-	{
+	if (out == NULL) {
 		printf("Unable to open output file\n");
 		return EXIT_FAILURE;
 	}
 
-	fillPrimes( arPrimes, NumPrimes );
+	fillPrimes(out, arPrimes, NUMPRIMES);
 
-	printf( "%d prime values found\n", NumPrimes );
+	printf("%d prime values found\n", NUMPRIMES);
+	// printArray(out, arPrimes, NUMPRIMES);
 	
-	printArray( out, arPrimes, NumPrimes );
-	
-	fclose( out );
+	fclose(out);
 }
 
-void printArray( FILE *out, int* array, int size )
-{
-	for( int ix=0; ix<size; ix++ )
-	{
+void printArray(FILE *out, int* array, int size) {
+	for( int ix=0; ix<size; ix++ ) {
 		fprintf( out, "%d\n", array[ ix ] );
 	}
 }
--- ho8/primes/primes.c
+++ ho8/primes/primes.c
@@ -1,26 +1,30 @@
-void fillPrimes( int* arPrimes, int size )
-{
+#include <stdio.h>
+
+void fillPrimes(FILE* out, int* arPrimes, int size) {
 	int primeCount = 0; 
-	int current = 2; 
+	int current = 2;
+	int isPrime = 1;
 	
-	while( primeCount < size )
-	{
-		int isPrime = 1; 
-		for( int ixPrime = 0;
-			     ixPrime < primeCount && arPrimes[ ixPrime ] <= current / arPrimes[ ixPrime ];
-				 ixPrime++ )
+	while(primeCount < size) {
+		isPrime = 1;
+		fprintf(out, "CURRENT: %d CHECKED:", current);
+
+		for(int ixPrime = 0;
+			   (ixPrime < primeCount) && (arPrimes[ixPrime] <= current/arPrimes[ixPrime]);
+				ixPrime++)
 		{
-			if( current % arPrimes[ ixPrime ] == 0 )
-			{
+			fprintf(out, " %d", arPrimes[ixPrime]);
+			if(current%arPrimes[ixPrime] == 0) {
 				isPrime = 0; 
 				break;
 			}
 		}
 		
-		if( isPrime )
-		{
-			arPrimes[ primeCount++ ] = current;
+		if(isPrime) {
+			arPrimes[primeCount++] = current;
+			fprintf(out, " PRIME");
 		}
+		fprintf(out, "\n");
 		current++;
 	}
 }
